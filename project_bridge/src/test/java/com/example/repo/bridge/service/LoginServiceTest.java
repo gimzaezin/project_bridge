20fb087515eb02e736ace8d6554bc304ad4ae0c4
gimzaezin <kimjaejin@chosun.kr>
2024-05-06 22:58:14 +0900

    update logintest 로그인테스트 로그추가, 아이디만틀렸을때, 비밀번호만틀렸을때, 둘다틀렸을때 경우의 로그인테스트 업데이트

--- project_bridge/src/test/java/com/example/repo/bridge/service/LoginServiceTest.java
+++ project_bridge/src/test/java/com/example/repo/bridge/service/LoginServiceTest.java
@@ -1,30 +1,26 @@
 package com.example.repo.bridge.service;
 
 
-import com.example.repo.bridge.domain.GenderCode;
 import com.example.repo.bridge.domain.User;
 import com.example.repo.bridge.repository.userrepository.DataJpaUserRepository;
 import com.example.repo.bridge.request.LoginRequest;
-import com.example.repo.bridge.request.RegisterUserRequest;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestConstructor;
-import org.springframework.test.context.junit4.SpringRunner;
 
-import java.time.LocalDate;
-
+import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 
 @SpringBootTest
 @TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
 public class LoginServiceTest {
+    private static final Logger log = LoggerFactory.getLogger(LoginServiceTest.class);
     @Autowired
     private DataJpaUserRepository userRepository;
 
@@ -32,17 +28,44 @@ public class LoginServiceTest {
     private LoginService loginService;
 
     @Test
-    public void loginTest() {
+    public void loginTest() {   //db에 있는 아이디 비밀번호로 정상로그인
         LoginRequest loginRequest = new LoginRequest();
         loginRequest.setLoginId("kimjaejin");
         loginRequest.setPassword("1234");
 
-        User loginuser= loginService.login(loginRequest);
-
+        User loginuser = loginService.login(loginRequest);
+        log.info(loginuser.getLoginId());
+        log.info(loginuser.getPassword());
         assertNotNull(loginuser);
     }
-}
 
+    @Test
+    public void incorrectIdTest() { //아이디 틀렸을때 테스트
+        LoginRequest loginRequest = new LoginRequest();
+        loginRequest.setLoginId("incorrectID");
+        loginRequest.setPassword("1234");
+        User loginuser = loginService.login(loginRequest);
+        assertNull(loginuser);
+    }
+
+    @Test
+    public void incorrectPasswordTest() {   //비밀번호 틀렸을때 테스트
+        LoginRequest loginRequest = new LoginRequest();
+        loginRequest.setLoginId("kimjaejin");
+        loginRequest.setPassword("incorrectPassword");
+        User loginuser = loginService.login(loginRequest);
+        assertNull(loginuser);
+    }
+
+    @Test
+    public void incorrectIdPasswordTest() {   //아이디 비밀번호 둘다 틀렸을때 테스트
+        LoginRequest loginRequest = new LoginRequest();
+        loginRequest.setLoginId("incorrectID");
+        loginRequest.setPassword("incorrectPassword");
+        User loginuser = loginService.login(loginRequest);
+        assertNull(loginuser);
+    }
+}
 //INSERT INTO user_table (user_id, login_id, password, name, email, birth, introduction, nickname, state, gender_code)
 //    VALUES
 //    (1, 'kimjaejin', '1234', '김재진', 'kimjaejin@example.com', '2000-01-01', 'Hello, I am John Doe.', 'kim', 1, 'MALE');
