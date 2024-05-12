7879d038b83fe8ef3fc5a33d35732ebb6c0e40bb
gimzaezin <kimjaejin@chosun.kr>
2024-05-12 23:19:24 +0900

    Create RegisterController, register.http Update RegisterService, RegisterServiceTest, RegisterUserRequest, User, WebConfig
    레지스터 컨트롤러 작성, 레지스터 테스트용 post http
    레지스터 서비스 save->signUp 함수이름변경
    레지스터유저리퀘스트 notnull User도메인으로 이동
    Userdomain 제약조건 추가

--- project_bridge/src/main/resources/sql/ddl.sql
+++ project_bridge/src/main/resources/sql/ddl.sql
@@ -1,5 +1,5 @@
-# INSERT INTO bridge.user (birth, score, state, email, introduction, login_id, name, nickname, password, gender_code) VALUES ('2024-05-23', 80, 1, 'wqe', 'qwe', 'qwe', '123', 'ads', 'dg', 'MALE');
-# INSERT INTO bridge.user (birth, score, state, email, introduction, login_id, name, nickname, password, gender_code) VALUES ('2024-05-02', 24, 1, 'sdfsdf', 'wtesfd', 'fvbc', 'ertert', 'cvb', 'dfg', 'FEMALE');
-# INSERT INTO bridge.post (state, view_count, created_at, end_time, start_time, user_id, content, title) VALUES (1, 21, '2024-05-12 15:40:21.000000', '2024-05-12 15:40:28.000000', '2024-05-12 15:40:31.000000', 1, 'asd', 'qweqe');
-# INSERT INTO bridge.template (score, content) VALUES (2, '이 집 잘하네');
-# INSERT INTO bridge.template (score, content) VALUES (10, '밥경찰');
+INSERT INTO bridge.user (birth, score, state, email, introduction, login_id, name, nickname, password, gender_code) VALUES ('2024-05-23', 80, 1, 'wqe', 'qwe', 'qwe', '123', 'ads', 'dg', 'MALE');
+INSERT INTO bridge.user (birth, score, state, email, introduction, login_id, name, nickname, password, gender_code) VALUES ('2024-05-02', 24, 1, 'sdfsdf', 'wtesfd', 'fvbc', 'ertert', 'cvb', 'dfg', 'FEMALE');
+INSERT INTO bridge.post (state, view_count, created_at, end_time, start_time, user_id, content, title) VALUES (1, 21, '2024-05-12 15:40:21.000000', '2024-05-12 15:40:28.000000', '2024-05-12 15:40:31.000000', 1, 'asd', 'qweqe');
+INSERT INTO bridge.template (score, content) VALUES (2, '이 집 잘하네');
+INSERT INTO bridge.template (score, content) VALUES (10, '밥경찰');
